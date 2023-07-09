94769391a3ebbca504ff1b2fa15c6ff0ad675a20
Victor Gonzalez <victor.vgonzalez@mercadolibre.com>
2023-07-09 18:23:10 -0300

    Camara con auto-foco implementada

--- app/src/main/java/com/example/litabot/MainActivity.kt
+++ app/src/main/java/com/example/litabot/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.litabot
 import android.Manifest
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
+import android.graphics.Rect
 import android.hardware.Camera
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -16,6 +20,8 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
     private lateinit var surfaceView: SurfaceView
     private lateinit var surfaceHolder: SurfaceHolder
     private var camera: Camera? = null
+    private lateinit var captureButton: Button
+    private lateinit var anotherButton: Button
 
     private val CAMERA_PERMISSION_REQUEST_CODE = 1001
 
@@ -29,6 +35,77 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
 
         // Set the display orientation to portrait
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        captureButton = findViewById<Button>(R.id.btnCapture)
+        anotherButton = findViewById<Button>(R.id.anotherButton)
+        captureButton.setOnClickListener {
+            takePicture()
+            captureButton.visibility = View.GONE
+            anotherButton.visibility = View.VISIBLE
+        }
+        anotherButton.setOnClickListener {
+            startCameraPreview()
+            anotherButton.visibility = View.GONE
+            captureButton.visibility = View.VISIBLE
+        }
+        surfaceView.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val focusRect = calculateFocusArea(event.x, event.y) // Calculate the focus area based on the touch coordinates
+                performAutoFocus(focusRect) // Trigger autofocus using the focus area
+            }
+            true
+        }
+    }
+
+    private fun calculateFocusArea(x: Float, y: Float): Rect {
+        val previewSize = camera?.parameters?.previewSize
+        previewSize?.let {
+            val focusRectSize = 200 // Size of the focus area rectangle in pixels (adjust as needed)
+
+            val centerX = x / it.width
+            val centerY = y /it.height
+
+            val left = clamp(centerX - 0.5f, -0.5f, 0.5f) * 2000 + 1000
+            val top = clamp(centerY - 0.5f, -0.5f, 0.5f) * 2000 + 1000
+            val right = left + focusRectSize
+            val bottom = top + focusRectSize
+
+            return Rect(
+                (left - 1000).toInt(),
+                (top - 1000).toInt(),
+                (right - 1000).toInt(),
+                (bottom - 1000).toInt()
+            )
+        }
+        return Rect(-1000, -1000, 1000, 1000)
+    }
+
+    private fun clamp(value: Float, min: Float, max: Float): Float {
+        return Math.max(min, Math.min(max, value))
+    }
+
+    private fun performAutoFocus(focusRect: Rect) {
+        val parameters = camera?.parameters
+        parameters?.let {
+            if (it.focusMode == Camera.Parameters.FOCUS_MODE_AUTO ||
+                it.focusMode == Camera.Parameters.FOCUS_MODE_MACRO
+            ) {
+                it.focusMode = Camera.Parameters.FOCUS_MODE_AUTO
+                if (it.maxNumFocusAreas > 0) {
+                    val focusAreas = mutableListOf<Camera.Area>()
+                    focusAreas.add(Camera.Area(focusRect, 1000))
+                    it.focusAreas = focusAreas
+                }
+                camera?.cancelAutoFocus()
+                camera?.autoFocus { success, _ ->
+                    if (success) {
+                        // Focus operation succeeded
+                    } else {
+                        // Focus operation failed
+                    }
+                }
+            }
+        }
     }
 
     override fun onResume() {
@@ -37,6 +114,16 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
             startCameraPreview()
         } else {
             requestCameraPermission()
+        }
+    }
+
+    private fun takePicture() {
+        camera?.takePicture(null, null, PictureCallback())
+    }
+
+    private inner class PictureCallback : Camera.PictureCallback {
+        override fun onPictureTaken(data: ByteArray?, camera: Camera?) {
+            // Process the captured image data here
         }
     }
 
@@ -65,9 +152,13 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
         try {
             camera = Camera.open()
             val parameters = camera?.parameters
-            parameters?.setRotation(90)
-            camera?.setDisplayOrientation(90)
-            camera?.parameters = parameters
+            parameters?.let {
+                it.setRotation(90)
+                camera?.setDisplayOrientation(90)
+                camera?.parameters = it
+                val previewSize = it.previewSize
+                surfaceView.layoutParams.height = previewSize.width
+            }
             camera?.setPreviewDisplay(surfaceHolder)
             camera?.startPreview()
         } catch (e: Exception) {
